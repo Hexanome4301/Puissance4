f0b8c3e8e3136f5cdd09aeb7fc9fee2203efe6bc
Jaf270 <thomas.losbar@gmail.com>
2014-10-23 13:23:21 +0200

    minMax amélioré (2)

--- minMax.pl
+++ minMax.pl
@@ -2,14 +2,11 @@
 %ou un desavantage, la regle renvoie un certain score
 eval(GamestateEnCours,Joueur,Score):-
 	solution(Qui,GamestateEnCours),  %on demande si il y a un vainqueur et de le mettre dans Qui
-	(
+
 	%on compare si c'est notre Joueur
 	%si c'est oui on donne un gros score positif
-	(Qui = Joueur , Score = 100 , ! );
-
-	%la on regarde si c'est l'adversaire qui a gagner
-	%si c'est oui on donne un scrore negatif
-	(not(Qui = Joueur),Score = -100 , ! )).
+	%si c'est non on donne un scrore negatif
+	(	(Qui = Joueur , Score = 100 , ! );	(not(Qui = Joueur),Score = -100 , ! )	).
 
 eval(GamestateEnCours,_,Score):-
 	%supposons que solution renvoie true et le nom du vainqueur quand on lui demande et false sinon
@@ -23,160 +20,143 @@ eval(GamestateEnCours,_,Score):-
 % C'est a partir de ce nouvel etat de jeu que l'adversaire devra jouer un coup. Il cherchera donc a minimiser notre score.
 % Parmi tous les score min (proposé par l'adversaire) qui s'offrent à l'IA,
 % faudra qu'il choisisse le plus élevé (car il veut maximiser son score).
-iaMinMax(Joueur,CoupDAvance,MeilleurCoup,LigneDuNouvelElem):-
+iaMinMax(Joueur,CoupDAvance,MeilleurCoup,LigneDuNouvelElem,ListPoids):-
 	gamestate(X),
-	retract(gamestateTampon(_)),
-	assert(gamestateTampon(X)),
 
-	retract(gamestateTampon2(_)),
-	assert(gamestateTampon2(X)),
-
-	retract(gamestateTampon3(_)),
-	assert(gamestateTampon3(X)),
+	init_gamestate,
 
 	coupPossible(X,ListeDeCoup),
 
-	% ici une regle recursive sur la liste de coupPossible.
-	%Fais jouer un coup, regarde le poid du coup prochain et compare les deux poids et renvoie le meilleur poids
-	simul(Joueur,CoupDAvance,ListeDeCoup,Poids,MeilleurCoup),
-	joueIa(Joueur,MeilleurCoup,Poids,LigneDuNouvelElem).
+	%Simul tous les coup possible et renvoit le meilleur coup a jouer ainsi que la valeur de son poids
+	simul(Joueur,CoupDAvance,ListeDeCoup,ListPoids,MeilleurCoup),
+	joueIa(Joueur,MeilleurCoup,ListPoids,LigneDuNouvelElem).
 
-joueIa(Joueur,MeilleurCoup,Poids,LigneDuNouvelElem):-
-	(Poids=0 , iaRandom(Joueur,Coup,LigneDuNouvelElem));
-	(not(Poids=0), jouer(Joueur,MeilleurCoup,_,LigneDuNouvelElem)).
+% Determine si l'IA doit jouer au milieu pour un premier tour, jouer de façon prémédité ou bien de façon aléatoire
+joueIa(Joueur,_,ListPoids,LigneDuNouvelElem):-
+	subtract(ListPoids,[-1000000],TrueListPoids),
+	gamestate(X),subtract(TrueListPoids,[0],[]),X=[[],[],[],[],[],[],[]],jouer(Joueur,4,LigneDuNouvelElem), ! .
+
+joueIa(Joueur,_,ListPoids,LigneDuNouvelElem):-
+	subtract(ListPoids,[-1000000],TrueListPoids),
+	gamestate(X),subtract(TrueListPoids,[0],[]),not(X=[[],[],[],[],[],[],[]]),iaRandom(Joueur,_,LigneDuNouvelElem),!.
+
+joueIa(Joueur,MeilleurCoup,ListPoids,LigneDuNouvelElem):-
+	subtract(ListPoids,[-1000000],TrueListPoids),
+	not(subtract(TrueListPoids,[0],[])), jouer(Joueur,MeilleurCoup,LigneDuNouvelElem),!.
 
 %condition d'arret:
 %Lorsqu'il n'y a plus de coup possible a jouer -> [].
 %On choisit des valeur par defaut pour 'Poids' et 'Coup'.
 %Le Poids sera comparé à un vrai poids calculé à partir d'un coup de l'adversaire
-simul(_,_,[],Poids,Coup):-
-	Poids =  0,
-	Coup = 4.
+simul(_,_,[],Poids,4):-
+	Poids = [-1000000].
 
+simul(Joueur,ProfondeurEnCours,ListeDeCoup,ListPoids,MeilleurCoup):-
+	ListeDeCoup = [CoupActuel|Queue],
 
-simul(Joueur,ProfondeurEnCours,ListeDeCoup,MeilleurPoids,MeilleurCoup):-
-	ListeDeCoup = [Colonne|Queue],
-	simulationCoup(Joueur,Colonne,NewGamestate),
+	%sauvegarde du ctx actuel
+	gamestateTampon(GamestateEnCours),
+
+	simulationCoup(Joueur,CoupActuel,NewGamestate),
 
 	%sur la profondeur suivante, ça sera a l'adversaire de jouer et bien sur il jouera le coup donnant à l'IA le pire score
 	ProfondeurSuivante is ProfondeurEnCours-1,
-	min(NewGamestate,Joueur,ProfondeurSuivante,PoidsDuCoupActuel,_),
-	%ici en gros: sur le prochain tour de jeu ou l'etat de la grille sera NewGamestate, CoupEnnemi represente le pire coup contre
-	%l'ia que pourra jouer son adversaire et il aura un poids egal a PoidsDuCoupActuel
+	min(NewGamestate,Joueur,ProfondeurSuivante,PoidsCoupActuel),
 
 	%on revient a la configuration du jeu en cours pour simuler un prochain coup:
 	retract(gamestateTampon(_)),
-	gamestateTampon3(X), assert(gamestateTampon(X)),
-
-	CoupActuel = Colonne,
+	assert(gamestateTampon(GamestateEnCours)),
 
 	%on simule le prochain coup possible
-	simul(Joueur,ProfondeurEnCours,Queue,PoidsDuCoupSuivant,CoupSuivantAssocie),
+	simul(Joueur,ProfondeurEnCours,Queue,ListPoidsCoupsSuivants,CoupSuivantAssocie),
 
-	balanceMax(PoidsDuCoupActuel,PoidsDuCoupSuivant,MeilleurPoids),
-	meilleurCoup(PoidsDuCoupActuel,PoidsDuCoupSuivant,MeilleurPoids,CoupActuel,CoupSuivantAssocie,MeilleurCoup).
+	append([PoidsCoupActuel],ListPoidsCoupsSuivants,ListPoids),
+	meilleurCoup(ListPoids,PoidsCoupActuel,CoupActuel,CoupSuivantAssocie,MeilleurCoup).
+
+meilleurCoup(ListPoids,PoidsCoupActuel,CoupActuel,_,MeilleurCoup):-
+	max_member(MeilleurPoids,ListPoids),
+	PoidsCoupActuel = MeilleurPoids, MeilleurCoup = CoupActuel,!.
+
+meilleurCoup(ListPoids,PoidsCoupActuel,_,CoupSuivantAssocie,MeilleurCoup):-
+	max_member(MeilleurPoids,ListPoids),
+	not(PoidsCoupActuel = MeilleurPoids), MeilleurCoup = CoupSuivantAssocie,!.
 
 %Principe de min: A un certain etat/configuration du jeu (GamestateEnCours) pour la profondeur d'arbre 'Profondeur'
 % on renvoie le coup procurant un score Result minimum pour notre Joueur (en l'occurence l'IA) et correspondant au
 % coup 'Coup' pour l'adversaire (Colonne jouee)
 % NOTE: dans la realisation en s'en fout du Result c'est Coup qui sera interessant de recup pour la regle iaMinMax
-min(GamestateEnCours,Joueur,Profondeur, Result, Coup):-
+min(GamestateEnCours,Joueur,Profondeur, Result):-
 	not(Profondeur = 0),
 
 	% selon la configuration du jeu, on recupere la ListeDeCoup possible
 	coupPossible(GamestateEnCours,ListeDeCoup),
-	retract(gamestateTampon2(_)),
-	assert(gamestateTampon2(GamestateEnCours)),
+
 	% L'adversaire va simuler tout les coups possible et en prévoyant un nombre 'Profondeur'
 	% de coup d'avance va donner dans la variable Result le pire score (poids) pour son adversaire associe au coup Coup
-	simulAdversaire(Joueur,Profondeur,ListeDeCoup,Result,Coup).
+	simulAdversaire(Joueur,Profondeur,ListeDeCoup,ListPoids,_),
+	min_member(Result,ListPoids).
 
 %condition d'arret :
 %lorsqu'on atteint la profondeur 0, on evalue la situation du jeu
-min(GamestateEnCours,Joueur,0, Result, _ ):-
+min(GamestateEnCours,Joueur,0, Result):-
 	eval(GamestateEnCours,Joueur,Result).
 
 %condition d'arret :
 %-infini represente le pire poid possible pour commencer nos comparaison
 simulAdversaire(_,_,[],PirePoids,4):-
-	PirePoids = 0.
+	PirePoids = [1000000].
 
 % recursion interne dans min ou l'adversaire teste tous les coup possible et donne le poid du coup et le coup associe
-simulAdversaire(Joueur,ProfondeurEnCours,ListeDeCoup,PirePoids,PireCoup):-
-	ListeDeCoup = [Colonne|Queue],
-	%la c'est une regle comme jouer sauf qu'elle n'utilise pas la clause gamestate() qu'on a en dynamique mais un gamestate tampon
+simulAdversaire(Joueur,ProfondeurEnCours,ListeDeCoup,ListPoids,PireCoup):-
+	ListeDeCoup = [CoupActuel|Queue],
 	changerPion(Joueur,Adversaire),
-	simulationCoup(Adversaire,Colonne,NewGamestate), %adversaire de m (l'ia)
+
+	%sauvegarde du ctx actuel
+	gamestateTampon(GamestateEnCours),
+
+	simulationCoup(Adversaire,CoupActuel,NewGamestate),
 
 	%sur la profondeur suivante, ça sera a l'IA de jouer et bien sur il jouera le coup lui donnant le plus gros score
 	ProfondeurSuivante is ProfondeurEnCours-1,
-	max(NewGamestate,Joueur,ProfondeurSuivante,PoidsDuCoupActuel,_),
+	max(NewGamestate,Joueur,ProfondeurSuivante,PoidsCoupActuel),
 
 	%on revient a la configuration du jeu en cours:
 	retract(gamestateTampon(_)),
-	gamestateTampon2(X), assert(gamestateTampon(X)),
-
-	CoupActuel = Colonne,
+	assert(gamestateTampon(GamestateEnCours)),
 
 	%on simule le prochain coup possible
-	simulAdversaire(Joueur,ProfondeurEnCours,Queue,PoidsDuCoupSuivant, CoupSuivantAssocie),
+	simulAdversaire(Joueur,ProfondeurEnCours,Queue,ListPoidsCoupsSuivants, CoupSuivantAssocie),
 
-	balanceMin(PoidsDuCoupActuel,PoidsDuCoupSuivant,PirePoids),
-	%regle tres moche pour juste renvoye le PireCoup associe au PirePoids
-	pireCoup(PoidsDuCoupActuel,PoidsDuCoupSuivant,PirePoids,CoupActuel,CoupSuivantAssocie,PireCoup).
+	append([PoidsCoupActuel],ListPoidsCoupsSuivants,ListPoids),
+	pireCoup(ListPoids,PoidsCoupActuel,CoupActuel,CoupSuivantAssocie,PireCoup).
 
+pireCoup(ListPoids,PoidsCoupActuel,CoupActuel,_,PireCoup):-
+	min_member(PirePoids,ListPoids),
+	PoidsCoupActuel = PirePoids, PireCoup = CoupActuel,!.
 
-max(GamestateEnCours,Joueur,Profondeur, Result, Coup):-
+pireCoup(ListPoids,PoidsCoupActuel,_,CoupSuivantAssocie,PireCoup):-
+	min_member(PirePoids,ListPoids),
+	not(PoidsCoupActuel = PirePoids), PireCoup = CoupSuivantAssocie,!.
+
+max(GamestateEnCours,Joueur,Profondeur, Result):-
 	not(Profondeur = 0),
 
 	% selon la configuration du jeu, on recupere la ListeDeCoup possible
 	coupPossible(GamestateEnCours,ListeDeCoup),
 
-	retract(gamestateTampon3(_)),
-	assert(gamestateTampon3(GamestateEnCours)),
-
 	% L'IA va simuler tout les coups possible et en prévoyant un nombre 'Profondeur'
 	% de coup d'avance va donner dans la variable Result le meilleur score (poids) associe au coup 'Coup'
-	simul(Joueur,Profondeur,ListeDeCoup,Result,Coup).
+	simul(Joueur,Profondeur,ListeDeCoup,ListPoids,_),
+	max_member(Result,ListPoids).
 
 %condition d'arret: lorsque max recoit en parametre la profondeur 0 c'est que l'adversaire ne doit plus regarder ce
 % que pourrait jouer l'ia et evalue pour lui même le score qu'il effectuerais pour ce(son) dernier tour de jeu prémédité
-max(GamestateEnCours,Joueur,0, Result,_):-
+max(GamestateEnCours,Joueur,0, Result):-
 	eval(GamestateEnCours,Joueur,Result).
 
 
 %Liste de regle fonctionnelle
-meilleurCoup(PoidsDuCoupActuel,PoidsDuCoupSuivant,PirePoids,CoupAssocie,_,MeilleurCoup):-
-	PoidsDuCoupActuel = PirePoids,
-	not(PoidsDuCoupActuel = PoidsDuCoupSuivant),
-	MeilleurCoup = CoupAssocie , ! .
-
-meilleurCoup(PoidsDuCoupActuel,PoidsDuCoupSuivant,PirePoids,_,CoupSuivantAssocie,MeilleurCoup):-
-	PoidsDuCoupSuivant = PirePoids,
-	MeilleurCoup = CoupSuivantAssocie, ! .
-
-pireCoup(PoidsDuCoupActuel,PoidsDuCoupSuivant,MeilleurCoup,CoupAssocie,_,PireCoup):-
-	PoidsDuCoupActuel = MeilleurCoup,
-	not(PoidsDuCoupActuel = PoidsDuCoupSuivant),
-	PireCoup = CoupAssocie , ! .
-
-pireCoup(PoidsDuCoupActuel,PoidsDuCoupSuivant,MeilleurCoup,_,CoupSuivantAssocie,PireCoup):-
-	PoidsDuCoupSuivant = MeilleurCoup,
-
-	PireCoup = CoupSuivantAssocie, ! .
-
-balanceMin(PoidsDuCoupActuel,PoidsDuCoupSuivant,PirePoids):-
-	PoidsDuCoupActuel < PoidsDuCoupSuivant,
-	PirePoids = PoidsDuCoupActuel, ! .
-
-balanceMin(PoidsDuCoupActuel,PoidsDuCoupSuivant,PirePoids):-
-	PoidsDuCoupActuel >= PoidsDuCoupSuivant,
-	PirePoids = PoidsDuCoupSuivant, ! .
-
-balanceMax(PoidsDuCoupActuel,PoidsDuCoupSuivant,PirePoids):-
-	PoidsDuCoupActuel > PoidsDuCoupSuivant,
-	PirePoids = PoidsDuCoupActuel, ! .
-
-balanceMax(PoidsDuCoupActuel,PoidsDuCoupSuivant,PirePoids):-
-	PoidsDuCoupActuel =< PoidsDuCoupSuivant,
-	PirePoids = PoidsDuCoupSuivant, ! .
+init_gamestate:-
+	gamestate(X),
+	retract(gamestateTampon(_)),
+	assert(gamestateTampon(X)).
